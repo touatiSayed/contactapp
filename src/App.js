763d3ebd30ea7da8c31d5a77e3f64b3d23025e2f
touatisayed <touati.sayed47@gmail.com>
2025-05-15 22:47:45 +0100

    Initialize App component with contact fetching logic using useEffect and state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-
+import { useEffect, useState } from 'react';
 import './App.css';
+import { getContacts } from './api/ContactService';
 
-function App() {
+
+function App () {
+  const [data, setData] = useState({});
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const getAllContacts = async (page = 0, size = 10) => {
+    try {
+      setCurrentPage(page);
+      const { data } = await getContacts(page, size);
+      setData(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getAllContacts();
+  }, []);
+
   return (
     <div>
       <h1>Miao Miao</h1>
